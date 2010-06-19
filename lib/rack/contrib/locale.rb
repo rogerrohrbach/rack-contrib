c95185b900f20f12d2be54b8a67cf280ef8584fd
Roger Rohrbach <roger@ecstatic.com>
2010-06-19 16:39:12 +0200

    Instead of setting the locale based on the first language detected in the Accept-Language header, rank the languages by preference and use the most suitable available one.
    Don't attempt to load i18n if I18n already defined (e.g., in the Rails
    environment).

--- lib/rack/contrib/locale.rb
+++ lib/rack/contrib/locale.rb
@@ -1,4 +1,4 @@
-require 'i18n'
+require 'i18n' unless defined? I18n
 
 module Rack
   class Locale
@@ -6,17 +6,13 @@ module Rack
       @app = app
     end
 
+
     def call(env)
       old_locale = I18n.locale
       locale = nil
 
-      # http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
       if lang = env["HTTP_ACCEPT_LANGUAGE"]
-        lang = lang.split(",").map { |l|
-          l += ';q=1.0' unless l =~ /;q=\d+\.\d+$/
-          l.split(';q=')
-        }.first
-        locale = lang.first.split("-").first
+        locale = locale_from_accept_language(lang)
       else
         locale = I18n.default_locale
       end
@@ -27,5 +23,48 @@ module Rack
       I18n.locale = old_locale
       [status, headers, body]
     end
+
+
+    def locale_from_accept_language(field)
+
+      match = rank_preferred_languages(field).detect do |l|
+        I18n.available_locales.include?(l) || l == :*
+      end
+
+      match == :* ? I18n.default_locale : match
+    end
+
+
+    private
+    def rank_preferred_languages(accept_language)
+
+      # http://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.10
+      primary_tag = subtag = '[A-Za-z]{1,8}'
+      language_tag = "#{primary_tag}(-#{subtag})*"
+
+      # http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4
+      language_range = "#{language_tag}|\\*"
+      quality_factor = ';q=(0(\.\d+)*|1)'
+
+      languages = {}
+
+      accept_language.split(/, */).each do |e|
+        if l = /\A#{language_range}/.match(e)
+        then
+          # discard any subtags
+          l = /\A#{primary_tag}/.match(l.to_s) unless l.to_s == '*'
+
+          if q = /#{quality_factor}\z/.match(e).to_a[1]
+          then
+            languages[l.to_s] = q.to_f
+          else
+            languages[l.to_s] = 1
+          end
+        end
+      end
+
+      languages.sort { |a,b| b[1 ]<=> a[1] }.collect{ |k, v| k.to_sym }
+    end
+
   end
 end
