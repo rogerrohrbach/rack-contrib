e866b03abeecc2aa2357b36e3a30e1584f3c4c2b
Roger Rohrbach <roger@ecstatic.com>
2010-06-20 02:15:58 +0200

    Now infers the locale from the subdomain in the request URI.  A request of the form <ISO 639-1 code>.<domain>, where the ISO code corresponds to an available locale, sets the locale.  If the request is for an unavailable locale, 404 Not Found is returned.  The locale for an unlocalized is determined from the Accept-Language header, as before; but the client is now redirected to the appropriate localized URL.
    Locale.tld_length must be set to the length of the top-level domain; e.g.,
    1 for .com, 2 for .co.uk.

--- lib/rack/contrib/locale.rb
+++ lib/rack/contrib/locale.rb
@@ -1,27 +1,130 @@
 require 'i18n' unless defined? I18n
 
+
 module Rack
   class Locale
+    # customize for the length of your top-level domain
+    @@tld_length = 1
+
+    def self.tld_length
+      @@tld_length
+    end
+
+
+    def self.tld_length=(n)
+      @@tld_length = n
+    end
+
+
     def initialize(app)
       @app = app
     end
 
 
     def call(env)
-      old_locale = I18n.locale
-      locale = nil
+      request = Request.new(env)
 
-      if lang = env["HTTP_ACCEPT_LANGUAGE"]
-        locale = locale_from_accept_language(lang)
+      old_locale = I18n.locale
+      
+      if localized? request
+        unless locale = get_locale_from_subdomain(request)
+          return Response.new(
+            "Not found (unsupported locale).\n",
+            404,
+            {'Content-Type' => 'text/plain'}
+          ).finish
+        end
       else
-        locale = I18n.default_locale
+        # choose the most appropriate locale
+        if header = env["HTTP_ACCEPT_LANGUAGE"]
+          locale = locale_from_accept_language(header)
+        else
+          locale = I18n.default_locale
+        end
       end
 
-      locale = env['rack.locale'] = I18n.locale = locale.to_s
-      status, headers, body = @app.call(env)
-      headers['Content-Language'] = locale
-      I18n.locale = old_locale
-      [status, headers, body]
+      if ((locale == I18n.default_locale &&
+           request.url == default_locale_url(request)) ||
+          (request.url == localized_url(request, locale)))
+
+        # set the locale to match the request URI
+        locale = env['rack.locale'] = I18n.locale = locale.to_s
+        status, headers, body = @app.call(env)
+        headers['Content-Language'] = locale
+        I18n.locale = old_locale
+        [status, headers, body]
+      else
+        # redirect to the appropriate URL
+        Response.new(
+          "Redirecting for language #{locale.to_s}...\n",
+          302,
+          {
+            'Content-Type' => 'text/plain',
+            'Location' =>
+              (locale == I18n.default_locale) ?
+                default_locale_url(request) :
+                localized_url(request, locale)
+          }
+        ).finish
+      end
+    end
+
+
+    private
+
+    def localized?(request)
+      s = request.host.split('.').first
+      iso_639_1_code?(s) || I18n.available_locales.include?(s.to_sym)
+    end
+
+
+    def get_locale_from_subdomain(request)
+      sym = request.host.split('.').first.to_sym
+      (I18n.available_locales.include? sym) ? sym : nil
+    end
+
+
+    def default_locale_url(request)
+      host_components = request.host.split('.')
+
+      if (request.host.split('.')[0..-(self.class.tld_length + 2)]).length > 0
+        if iso_639_1_code? host_components.first
+          host_components.shift
+        end
+
+        new_url = "#{request.scheme}://#{host_components.join('.')}"
+
+        unless ((request.scheme == 'http' && request.port == 80) ||
+                (request.scheme == 'https' && request.port == 443))
+          new_url << ":#{request.port}"
+        end
+
+        new_url << request.fullpath
+      else
+        request.url
+      end
+    end
+
+
+    def localized_url(request, locale)
+      host_components = request.host.split('.')
+
+      if (host_components.first == 'www' ||
+          I18n.available_locales.include?(host_components.first.to_sym))
+
+        host_components.shift
+      end
+
+      host_components.unshift locale.to_s
+
+      new_url = "#{request.scheme}://#{host_components.join('.')}"
+
+      unless ((request.scheme == 'http' && request.port == 80) ||
+              (request.scheme == 'https' && request.port == 443))
+        new_url << ":#{request.port}"
+      end
+
+      new_url << request.fullpath
     end
 
 
@@ -35,7 +138,6 @@ module Rack
     end
 
 
-    private
     def rank_preferred_languages(accept_language)
 
       # http://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.10
@@ -71,5 +173,20 @@ module Rack
       languages.sort { |a,b| b[1 ]<=> a[1] }.collect{ |k, v| k.to_sym }
     end
 
+
+    def iso_639_1_code?(string)
+      %W(
+        aa ab ae af ak am an ar as av ay az ba be bg bh bi bm bn bo
+        br bs ca ce ch co cr cs cu cv cy da de dv dz ee el en eo es
+        et eu fa ff fi fj fo fr fy ga gd gl gn gu gv ha he hi ho hr
+        ht hu hy hz ia id ie ig ii ik io is it iu ja jv ka kg ki kj
+        kk kl km kn ko kr ks ku kv kw ky la lb lg li ln lo lt lu lv
+        mg mh mi mk ml mn mr ms mt my na nb nd ne ng nl nn no nr nv
+        ny oc oj om or os pa pi pl ps pt qu rm rn ro ru rw sa sc sd
+        se sg si sk sl sm sn so sq sr ss st su sv sw ta te tg th ti
+        tk tl tn to tr ts tt tw ty ug uk ur uz ve vi vo wa wo xh yi
+        yo za zh zu
+      ).include? string
+    end
   end
 end
